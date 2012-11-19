08a2b3890d0983911ae257577deb3173e8c32d02
daewon <blueiur@gmail.com>
2012-11-19 13:39:17 +0900

    hah

--- webclient/share-ace.js
+++ webclient/share-ace.js
@@ -9,7 +9,7 @@
     lines = editorDoc.getLines(0, range.start.row);
     offset = 0;
     for (i = _i = 0, _len = lines.length; _i < _len; i = ++_i) {
-      line = lines[i];
+        line = lines[i];
       offset += i < range.start.row ? line.length : range.start.column;
     }
     return offset + range.start.row;
